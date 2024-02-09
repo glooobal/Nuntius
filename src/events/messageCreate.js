88cd18d896bbe93f6a1bd0464f1a9b4b3e0e2ade
Jakub <jakubgorzny@icloud.com>
2024-02-09 02:04:30 +0100

    fix: Text statistics system fix

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,5 +1,5 @@
 import { Events } from 'discord.js';
-import { addUserMessages } from '../database/text/userMessages.js';
+import { addUserMessages } from '../database/textStatistics/userMessages.js';
 
 export default {
   name: Events.MessageCreate,
